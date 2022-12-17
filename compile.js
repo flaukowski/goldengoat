f330e5a92f2312dbe69b83324fe7ff9471f45907
The Hand <41301908+flaukowski@users.noreply.github.com>
2022-12-17 09:44:05 -0600

    Needs refactoring
    Please rebuild from scratch

---
+++ compile.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
+
+//delete "build" folder
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath);
+
+//read 'goldengoat.sol' from the contracts folder
+const campaignPath = path.resolve(__dirname, "contracts", "GoldenGoat.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+
+//compile
+const output = solc.compile(source, 1).contracts;
+
+//point at the 'build' folder
+fs.ensureDirSync(buildPath);
+
+//iterate over the contract objects and create json files for each in the 'build' folder
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(":", "") + ".json"),
+    output[contract]
+  );
+}
